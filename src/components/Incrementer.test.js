48f317e2fa350453e13bbccca70296c0b9cbcc09
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-14 12:21:56 -0700

    second incrementer test passing

--- src/components/Incrementer.test.js
+++ src/components/Incrementer.test.js
@@ -7,6 +7,13 @@ describe('incrementer component', () => {
     const wrapper = shallow(<Incrementer />);
     expect(wrapper).toMatchSnapshot();
   });
+  
+  it('increments the count', () => {
+    const wrapper = shallow(<Incrementer />);
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state('count')).toEqual(1);
+  });
 });
 
 
