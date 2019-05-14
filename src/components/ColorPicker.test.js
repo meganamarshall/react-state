33562671fd8eb8b9d2be53bb84f36ecc00561fa0
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-14 11:19:51 -0700

    finished colorPicker tests

--- src/components/ColorPicker.test.js
+++ src/components/ColorPicker.test.js
@@ -7,4 +7,29 @@ describe('ColorPicker component', () => {
     const wrapper = shallow(<ColorPicker />);
     expect(wrapper).toMatchSnapshot();
   });
+  
+  it('changes to red on red click', () => {
+    const wrapper = shallow(<ColorPicker />);
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state('color')).toEqual('red');
+  });
+
+  it('changes to green on green click', () => {
+    const wrapper = shallow(<ColorPicker />);
+    wrapper.find('button').at(1).simulate('click');
+    expect(wrapper.state('color')).toEqual('green');
+  });
+
+  it('changes to blue on blue click', () => {
+    const wrapper = shallow(<ColorPicker />);
+    wrapper.find('button').at(2).simulate('click');
+    expect(wrapper.state('color')).toEqual('blue');
+  });
+  
+  it('changes div class name to clicked color', () => {
+    const wrapper = shallow(<ColorPicker />);
+    wrapper.setState({ color: 'blue' }, () => {
+      expect(wrapper.find('div').at(0).hasClass('blue')).toBeTruthy();
+    });
+  });
 });
