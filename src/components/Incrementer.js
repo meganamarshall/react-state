bddd3db121640c6ff3d14f51ac2d0fbddf146b04
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-14 11:59:50 -0700

    rendering Incrementer but not incrementing

---
+++ src/components/Incrementer.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+
+export default class Incrementer extends PureComponent {
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState(state => {
+      return { count: state.count += 1 };
+    });
+  }
+
+  render() {
+    const { count } = this.state;
+    return (
+      <>
+        <p>Current Count: {count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </>
+    );
+  }
+}
+
